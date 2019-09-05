0b63901d4ea733b42fba290f1b3ee6e57f397060
Connor Meth <connormeth27@gmail.com>
2019-09-05 20:39:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["blue", "orange", "green", "purple"]
-  add_element_to_end_of_array << "red"
+  array = ["blue", "green", "purple"]
+  array << "red"
 end
 
 
